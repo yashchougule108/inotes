178ba4aa918a1c174f18a358e88c89d068b70903
yash <yashchougule31@gmail.com>
2024-06-23 00:57:02 +0530

    mongoDb connection

---
+++ BACKEND/db.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+const mongoURL="mongodb://localhost:27017/?tls=false&readPreference=primary&directConnection=true";
+
+
+const connectToMongo= async ()=>{
+    mongoose.set('strictQuery',false)
+    mongoose.connect(mongoURL)
+    console.log('mongo connected');
+  
+}
+
+module.exports=connectToMongo;
